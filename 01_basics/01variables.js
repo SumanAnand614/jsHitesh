dd463514a1ccaaf3f6553d6091561abedb34fe19
Suman Anand <114142267+SumanAnand614@users.noreply.github.com>
2024-03-02 14:59:59 +0000

    variables done

--- 01_basics/01variables.js
+++ 01_basics/01variables.js
@@ -5,5 +5,6 @@ let emailId = "abhishe_Mishra85@gmail.com";
 var accountPass = "12345"
 accountCity = "Jaipur"
 accountState = "Rajsthan"
+emailId= "hhshsh@hotmail.com"
 // prefer not to use var brcause of scope and functional scope
 console.table([name,emailId,accountId,accountCity]);
---
+++ 01_basics/02variables.js
@@ -0,0 +1,17 @@
+/*
+null => standlone value , matlab ye ki ye null namak ek value hai , ye null string return karega 
+undefined => it means yet its value is not defined , only the variables is defined untill yet 
+
+
+code readibility should be high 
+for documentation refer mdn ,written by mozilla
+standard documentaion of js ==> tc39.es
+symbol => for uniqueness
+object => a data type 
+type of null is object 
+type of undefined is undefined 
+
+
+*/
+
+//alert(3+2);  this alert is different from the alert used in browser console 
