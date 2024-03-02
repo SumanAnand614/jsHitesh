3e70c8f92528b89cbfa89dfbfe690c376465b896
Suman Anand <114142267+SumanAnand614@users.noreply.github.com>
2024-03-02 14:29:20 +0000

    basics1

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+console.log("hello suman anand");
+let name = "abhishek mishra "
+const accountId = "123455"
+let emailId = "abhishe_Mishra85@gmail.com";
+var accountPass = "12345"
+accountCity = "Jaipur"
+accountState = "Rajsthan"
+// prefer not to use var brcause of scope and functional scope
+console.table([name,emailId,accountId,accountCity]);
